34898cfd7c4673aae047a8f581ba3dfd97b36ed1
Davide Bonifacio - Studiokey <web@davidebonifacio.it>
2016-11-11 17:43:26 +0100

    Feature: checking URLs not to be empty

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,11 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         it('have correct URL', function() {
+            allFeeds.forEach(function(feed) {
+              expect(feed.url.length).not.toBe(0);
+            });
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
